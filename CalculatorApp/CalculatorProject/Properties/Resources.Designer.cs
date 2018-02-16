24854ee6b474e2fc7138f5c16b045d8510e79069
Rasmus Thor Priisholm <rpriisholm@hotmailcom>
2018-02-16 23:30:01 +0100

    Basic

--- CalculatorApp/CalculatorProject/Properties/Resources.Designer.cs
+++ CalculatorApp/CalculatorProject/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CalculatorApp.Properties
+namespace CalculatorProject.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace CalculatorApp.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CalculatorApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CalculatorProject.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- CalculatorApp/CalculatorProject/Properties/Settings.Designer.cs
+++ CalculatorApp/CalculatorProject/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CalculatorApp.Properties
+namespace CalculatorProject.Properties
 {
 
 
